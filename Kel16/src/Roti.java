207ba6ecf374c19ac2fafd2f146982be0cf2ae3f
sɐʎlı <137293380+1Ly45@users.noreply.github.com>
2023-06-23 14:37:04 +0700

    Super class Roti

---
+++ Kel16/src/Roti.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public abstract class Roti {
+    abstract int tepungTerigu();
+    abstract int gulaPasir();
+    abstract int butter();
+    abstract int ragi();
+    abstract int susuBubuk();
+    abstract int susuCair();
+    abstract int telur();
+    abstract int esBatu();
+    abstract int totalBerat();
+}
+
